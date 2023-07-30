3367a06891a26f8b67df2c0e162742c6d9fb6323
SingularDevice <thesingulardevice@gmail.com>
2023-07-30 19:17:32 +0200

    chore: Initial commit: Download base app from spring initializer

---
+++ src/main/java/com/techtest/decskill/DecskillApplication.java
@@ -0,0 +1,13 @@
+package com.techtest.decskill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecskillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecskillApplication.class, args);
+	}
+
+}
